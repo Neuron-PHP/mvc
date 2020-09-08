6e495a8f6533be5742a8b0f4df16ba0de9128a02
Lee Jones <lee@dragonflyrg.com>
2020-09-08 07:55:32 -0400

    Completed html view rendering.

--- resources/views/layouts/default.php
+++ resources/views/layouts/default.php
@@ -1,2 +1,4 @@
 <h1>Test Layout</h1>
 <?php
+require_once( $View );
+?>
--- src/Mvc/Controllers/HttpCodes.php
+++ src/Mvc/Controllers/HttpCodes.php
@@ -6,6 +6,6 @@ class HttpCodes extends Base
 {
 	public function _404( array $Parameters )
 	{
-		return $this->renderHtml( $Parameters );
+		return $this->renderHtml( $Parameters, '404' );
 	}
 }
